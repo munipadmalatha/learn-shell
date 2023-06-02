1ce6254c397674fb6a2d42303b976d9077ebd045
= <mallikarjuna@enginuity.ai>
2023-06-02 09:19:43 +0800

    added colors for printing

--- print.sh
+++ print.sh
@@ -1,2 +1,8 @@
 echo 'my name is latha'
 echo "Todays date is $DATE" 
+echo -e "\e[31mHello world\e[0m" #Red
+echo -e "\e[32mHello world\e[0m" #Green
+echo -e "\e[33mHello world\e[0m" #Yellow
+echo -e "\e[34mHello world\e[0m" #Blue
+echo -e "\e[35mHello world\e[0m" # magenta
+echo -e "\e[36mHello world\e[0m" #cyan
